db187cc63d52101e0bdb26605377d1954f536bb6
Javakky <dlixk_qazwx_three@yahoo.co.jp>
2019-01-14 01:20:22 +0900

    コメントを付けた

--- src/main/java/LoadDll.java
+++ src/main/java/LoadDll.java
@@ -6,11 +6,25 @@ import java.util.Set;
 
 import static org.apache.commons.io.FilenameUtils.*;
 
+/**
+ * コマンドでもdllをシステムプロパティに追加しつつこれを使うとバグるとおもいます
+ */
 public class LoadDll {
 
+    /**
+     * システムプロパティに追加したdllを保存しておく(重複回避)
+     */
     private static final Set<String> setDll = new HashSet<>();
 
-    public static String setDll(String filepath) throws NoSuchFieldException, IllegalAccessException, IOException {
+    /**
+     * dllをシステムプロパティに追加する <br/>
+     * jar化することも考慮するため、dllを一端外のtmpファイルに保存してから追加している。<br/>
+     * リソースフォルダにdllは入れてね☆
+     * @param filepath リソースフォルダより下のパス(/resource/a/test.dllならa/test.dll)
+     * @return 追加したtmpファイルの絶対パス
+     * @throws IOException tmpファイルの作成失敗
+     */
+    public static String setDll(String filepath) throws IOException {
         filepath = System.getProperty("user.dir") + "\\" + filepath;
         if (setDll.contains(filepath)) return null;
         String filename = getName(filepath);
@@ -53,12 +67,21 @@ public class LoadDll {
         return temp.getName();
     }
 
+    /**
+     * dllファイルからInterfaceの実装オブジェクトをロードする。<br/>
+     * 内部で {@link LoadDll#setDll(String)}を呼ぶので、他でシステムプロパティに追加するのはやめてね☆
+     * @param filename dllファイルのリソースフォルダより下のパス(/resource/a/test.dllならa/test.dll)
+     * @param clazz 生成したいInterfaceのクラス(jextract等で生成したやつ)
+     * @param <T> 生成したいInterface
+     * @return 生成したオブジェクト
+     */
+
     public static <T> T loadDll(String filename, Class<T> clazz) {
 
         String tmpFileName = null;
         try {
             tmpFileName = setDll(filename);
-        } catch (NoSuchFieldException | IllegalAccessException | IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
